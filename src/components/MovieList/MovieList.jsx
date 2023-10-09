48703a6870c8883f7e0083ccd9226a8d41b8275b
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-10-09 13:15:01 -0500

    added sweet alert for delete function

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -9,13 +9,14 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { makeStyles } from "@mui/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Swal from "sweetalert2";
 
 export default function MovieList() {
   const history = useHistory();
   const dispatch = useDispatch();
   // getting the movieList from moviesReducer
   const movieList = useSelector((store) => store.moviesReducer);
-// useEffect to fetch all movies. This goes to the fetchMovies saga and sends a put call to teh moviesReducer
+  // useEffect to fetch all movies. This goes to the fetchMovies saga and sends a put call to teh moviesReducer
   useEffect(() => {
     dispatch({ type: "FETCH_MOVIES" });
   }, []);
@@ -26,7 +27,7 @@ export default function MovieList() {
       width: "230px",
       height: "400px",
       marginBottom: "40px",
-      backgroundColor: 'black',
+      backgroundColor: "black",
       borderRadius: "5px",
       boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
       cursor: "pointer",
@@ -59,23 +60,23 @@ export default function MovieList() {
       alignItems: "center",
       justifyContent: "center",
       opacity: 0,
-      padding: '10px',
+      padding: "10px",
       transition: "opacity 0.3s ease-in-out",
     },
-    
+
     // Apply this class when the card is hovered to show the title
     cardHovered: {
       "&:hover": {
         "& $titleOnHover": {
           visibility: "visible",
-          opacity: '.7',
+          opacity: ".7",
         },
       },
     },
   });
   // variable for the styling
   const classes = useStyles();
-// what is returned on the page. Cards of all the movies that
+  // what is returned on the page. Cards of all the movies that
   return (
     <main>
       <br />
@@ -89,7 +90,7 @@ export default function MovieList() {
         Add Movie
       </button>
       <br />
-      <hr /> 
+      <hr />
       <br />
       <section className={classes.movieContainer}>
         {movieList.map((movie) => {
@@ -102,7 +103,7 @@ export default function MovieList() {
                 onClick={() => history.push(`/details/${movie.id}`)}
                 className={`${classes.card} ${classes.cardHovered}`}
               >
-                <CardMedia 
+                <CardMedia
                   component="img"
                   className={classes.image}
                   alt={movie.title}
@@ -110,8 +111,13 @@ export default function MovieList() {
                   title={movie.title}
                 />
                 <CardContent>
-                  <Typography gutterBottom variant="h5" component="div" style={{fontSize: '16px', color: 'white' }} >
-                  <span className={classes.titleOnHover}>{movie.title}</span>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    style={{ fontSize: "16px", color: "white" }}
+                  >
+                    <span className={classes.titleOnHover}>{movie.title}</span>
                   </Typography>
                 </CardContent>
               </Card>
@@ -120,9 +126,26 @@ export default function MovieList() {
                 color="error"
                 style={{ marginTop: "-30px", marginBottom: "40px" }}
                 startIcon={<DeleteIcon />}
-                onClick={() =>
-                  dispatch({ type: "DELETE_MOVIE", payload: movie.id })
-                }
+                onClick={() => {
+                  Swal.fire({
+                    title: "Are you sure?",
+                    text: "You will not be able to recover this movie!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!",
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      dispatch({ type: "DELETE_MOVIE", payload: movie.id });
+                      Swal.fire(
+                        "Deleted!",
+                        "The movie has been deleted.",
+                        "success"
+                      );
+                    }
+                  });
+                }}
               >
                 Delete
               </Button>
@@ -133,4 +156,3 @@ export default function MovieList() {
     </main>
   );
 }
-
